51736ed869c6c612288050ea07ca8daddc4e8bbb
NA Flipper <dioncleung@gmail.com>
2020-04-02 23:21:16 -0700

    built shell template for a custom readme file based off of user input

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const inquirer = require(`inquirer`)
 const axios = require(`axios`)
 const fs = require(`fs`);
 const { readMeTemplate } = require(`./writeReadMe.js`)
+require("dotenv").config();
+const authToken = process.env.API_KEY;
 
 inquirer
     .prompt([
@@ -13,23 +15,90 @@ inquirer
         },
         {
             type: `input`,
+            message: `What is your email address?`,
+            name: `email`
+        },
+        {
+            type: `input`,
+            message: `What is your app called?`,
+            name: `title`
+        },
+        {
+            type: `input`,
             message: `Please provide a short description of your app`,
             name: `description`
         },
-        // {
-        //     type: `input`,
-        //     message: `Please provide a short description of your app`,
-        //     name: `description`
-        // },
+        {
+            type: `input`,
+            message: `Are there any special installation instructions to use your app? If no, please leave this blank.`,
+            name: `installation`
+        },
+        {
+            type: `input`,
+            message: `Please provide a short set of instructions on how to use your app.`,
+            name: `usage`
+        },
+        {
+            type: `checkbox`,
+            message: `Please select all applicable open-source licenses.`,
+            name: `license`,
+            choices: [`MIT`, `BSD`, `Apache`]
+        },
+        {
+            type: `input`,
+            message: `Who else contributed towards the creation of this app?`,
+            name: `contributors`
+        },
+        {
+            type: `input`,
+            message: `What sort of test driven development has been incorporated in your app?`,
+            name: `tests`
+        },
+        {
+            type: `input`,
+            message: `What questions do you have?`,
+            name: `questions`
+        },
+        {
+            type: `input`,
+            message: `Anything else you would like a user to know?`,
+            name: `misc`
+        },
     ]).then(function(data){
-        // console.log(data)
-        // console.log(data.githubUsername)
         const githubUsername = data.githubUsername
+        const userEmail = data.email;
+        const userTitle = data.title;
+        const userDescription = data.description;
+        const userInstall = data.installation;
+        const userUsage = data.usage;
+        const userLicense = data.license;
+        const userContrib = data.contributors;
+        const userTests = data.tests;
+        const userQuestions = data.questions;
+        const userMisc = data.misc;
+
         axios
+            // .get(`-H "Authorization: token " https://api.github.com/users/${githubUsername}`).then(function(githubResponse){
             .get(`https://api.github.com/users/${githubUsername}`).then(function(githubResponse){
                 const githubData = githubResponse.data
-                console.log(githubData.email)
+                // console.log(githubData)
+                const profilePicURL = githubData.avatar_url
+                // console.log(profilePicURL)
+                return profilePicURL;
+            }).then(function(profilePicURL){
+                const filledReadMe = readMeTemplate(githubUsername, userEmail, userTitle, userDescription, userInstall, userUsage, userLicense, userContrib, userTests, userQuestions, userMisc, profilePicURL)
+                fs.writeFile(`README_${userTitle}.md`, filledReadMe, function (error){
+                    if (error) {
+                        console.log(error)
+                    } else {
+                        console.log("Successfully wrote a custom README.md")
+                    }
+                })
             })
+
+
+
+
         //     .get(GET /users/:username)
         //     .then(function(githubResponse){
         //         console.log(githubResponse)
